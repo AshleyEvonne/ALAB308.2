658eed3206393bc32947cf63eb414c723278e4c6
AshleyEvonne <ashleyevonne345@gmail.com>
2024-05-09 17:35:52 -0400

    barely got anywhere

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const plantRadius = 5
+//Setting my variables?
+
+const gardenRadius = 5
 const PI = 3.1415
-const area= PI *plantRadius* plantRadius;
+const area = PI * gardenRadius * gardenRadius;
+const minSpace = 0.8;
+const nPlants = 20;
+const week1Growth = 20;
+const week2Growth = week1Growth* 2;
+const week3Growth = week2Growth* 2;
+// plants should double
+
+const capacity = minSpace * nPlants;
+// const doubling = (nPlants * week1) && (week2 * nPlants) && (week3 * nPlants);
+console.log (area);
